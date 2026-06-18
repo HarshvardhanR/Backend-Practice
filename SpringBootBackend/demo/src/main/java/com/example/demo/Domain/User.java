e918283ca0cd79371addb2a45c99970742ac4d57
Harshvardhansingh Rao <harsh@harshvainghsair.home>
2026-06-17 20:58:25 -0400

    added some changes

---
+++ SpringBootBackend/demo/src/main/java/com/example/demo/Domain/User.java
@@ -0,0 +1,11 @@
+package com.example.demo.Domain;
+
+public class User{
+    int id;
+    String name;
+
+    public User(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
